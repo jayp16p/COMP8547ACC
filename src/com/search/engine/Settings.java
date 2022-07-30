0cb230ce57635fa790d91b35328a3c73246ee666
Jay Patel <showstepper16@gmail.com>
2022-07-30 18:47:23 -0400

    update

--- src/com/search/engine/Settings.java
+++ src/com/search/engine/Settings.java
@@ -8,7 +8,7 @@ public class Settings {
     public static final String TEXT_PATH = "Files"+ File.separator+ "TextFiles"; 
     public static final String  TAG = "1.0.0";
     public static final Integer MAX_DEPTH = 2;
-    public static final String CHARSET_NAME = "UTF-8"; // other wise BoyerMoore search wont work
+    public static final String CHARSET_NAME = "UTF-8"; // otherwise BoyerMoore search wont work
     public static final String DELIMITER = "::";
     public static final Integer RadixSize = 255; // for BoyerMoore search taking ascii format
     public static final Integer NumberOfSearch = 5;
